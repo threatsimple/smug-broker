659cdfcd9af24c63055ab9655f6cb700f4955a55
Jeremy Kelley <jeremy@33ad.org>
2019-10-12 23:21:01 -0500

    another bogus post to trigger slack integration. so sorry all

--- smug/slack.go
+++ smug/slack.go
@@ -278,6 +278,7 @@ func (sb *SlackBroker) Publish(ev *Event, dis Dispatcher) {
 
 // accept a slack string and remove urls in favor of url descr where available
 func (sb *SlackBroker) SimplifyParse(s string) string {
+    fmt.Printf("\n\nstarting SimplifyParse with\n%s\n\n", s)
     matches := sb.re_embeddedurls.FindAllStringSubmatchIndex(s,-1)
     // start at the end for replacement, this is a bit janky. XXX
     for i := len(matches)-1; i >= 0; i-- {
@@ -302,6 +303,7 @@ func (sb *SlackBroker) SimplifyParse(s string) string {
         }
         s = strings.ReplaceAll(s, entire_url, udescr)
     }
+    fmt.Printf("\n\nending SimplifyParse with\n%s\n\n", s)
     return s
 }
 
